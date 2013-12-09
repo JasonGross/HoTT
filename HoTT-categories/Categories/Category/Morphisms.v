e6a88e89343cf9fc947fc8a1d14428893654a31a
Jason Gross <jgross@mit.edu>
2013-12-08 19:36:53 -0500

    Remove things about to be ported

--- HoTT-categories/Categories/Category/Morphisms.v
+++ HoTT-categories/Categories/Category/Morphisms.v
@@ -15,32 +15,3 @@ Ltac find_composition_to_identity :=
                | let H'T := type of H' in fail 2 "error in rewriting a found identity" H "[" H'T "]"
                ]; clear H'
   end.
-
-
-Section AssociativityComposition.
-  Variable C : PreCategory.
-  Variables x0 x1 x2 x3 x4 : C.
-
-  Lemma compose4associativity_helper
-    (a : Morphism C x3 x4) (b : Morphism C x2 x3)
-    (c : Morphism C x1 x2) (d : Morphism C x0 x1)
-  : (a ∘ b) ∘ (c ∘ d) = (a ∘ ((b ∘ c) ∘ d)).
-  Proof.
-    rewrite !Associativity; reflexivity.
-  Qed.
-
-  Lemma compose4associativity_helper2
-    (a : Morphism C x3 x4) (b : Morphism C x2 x3)
-    (c : Morphism C x1 x2) (d : Morphism C x0 x1)
-  : a ∘ b ∘ c ∘ d = (a ∘ ((b ∘ c) ∘ d)).
-  Proof.
-    rewrite !Associativity; reflexivity.
-  Qed.
-End AssociativityComposition.
-
-Ltac compose4associativity' a b c d := transitivity (a ∘ ((b ∘ c) ∘ d)); try solve [ apply compose4associativity_helper ].
-Ltac compose4associativity :=
-  match goal with
-    | [ |- (?a ∘ ?b) ∘ (?c ∘ ?d) = _ ] => compose4associativity' a b c d
-    | [ |- _ = (?a ∘ ?b) ∘ (?c ∘ ?d) ] => compose4associativity' a b c d
-  end.
