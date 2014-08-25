842e49d10beb086748ca3a31cc907d0637f69f79
Jason Gross <jgross@mit.edu>
2014-08-24 21:43:04 -0400

    WIP

--- theories/categories/MonoidalCategory.v
+++ theories/categories/MonoidalCategory.v
@@ -3,6 +3,9 @@ Require Import Category.Prod Functor.Prod.
 Require Import FunctorCategory.Morphisms.
 Require Import Functor.Composition.Core.
 Require Import Functor.Identity.
+Require Import ProductLaws.
+Require Import InitialTerminalCategory.
+Require ExponentialLaws.Law1.Functors.
 
 Set Universe Polymorphism.
 Set Implicit Arguments.
@@ -16,6 +19,8 @@ Local Open Scope functor_scope.
 Local Open Scope morphism_scope.
 
 Section PreMonoidalPreCategory.
+  (** TODO: Redefine [NaturalIsomorphism] so it doesn't need [Funext]. *)
+  Context `{Funext}.
   (* It's too hard to implement it all inside a record, so first we
      define everything, then we put it in a record *)
 
@@ -33,112 +38,41 @@ Section PreMonoidalPreCategory.
   Let dst {s d} (_ : morphism C s d) := d.
 
   Local Notation "A ⊗ B" := (tensor_product (A%object, B%object)%core) (at level 40, left associativity).
-  Local Notation "A ⊗m B" := (morphism_of tensor_product (s := (@src _ _ A, @src _ _ B)) (d := (@dst _ _ A, @dst _ _ B)) (A%morphism, B%morphism)) (at level 40, left associativity).
+  Local Notation "A ⊗m B" := (morphism_of tensor_product
+                                          (s := (@src _ _ A, @src _ _ B))
+                                          (d := (@dst _ _ A, @dst _ _ B))
+                                          (A%morphism, B%morphism)) (at level 40, left associativity).
 
-  Let tri_monoidal_product_L_object_of (abc : C * C * C) : C :=
-    (fst_type (fst_type abc) ⊗ snd_type (fst_type abc)) ⊗ snd_type abc.
+  Local Open Scope functor_scope.
 
-  Let tri_monoidal_product_R_object_of (abc : C * C * C) : C :=
-    fst_type (fst_type abc) ⊗ (snd_type (fst_type abc) ⊗ snd_type abc).
-
-  Let tri_monoidal_product_L_morphism_of (s d : C * C * C) (m : morphism (C * C * C) s d)
-  : morphism C (tri_monoidal_product_L_object_of s) (tri_monoidal_product_L_object_of d)
-    := morphism_of tensor_product
-                   (s := (_, _)%core)
-                   (d := (_, _)%core)
-                   ((tensor_product _1 (fst_type (fst_type m), snd_type (fst_type m))%core)%morphism
-                    , snd_type m)%core.
-
-  Let tri_monoidal_product_R_morphism_of (s d : C * C * C) (m : morphism (C * C * C) s d)
-  : morphism C (tri_monoidal_product_R_object_of s) (tri_monoidal_product_R_object_of d).
-  Proof.
-      refine (morphism_of tensor_product
-                   (s := (_, _)%core)
-                   (d := (_, _)%core)
-                   (_
-                    , (morphism_of tensor_product
-                                   (s := (_, _)%core)
-                                   (d := (_, _)%core)
-                                   (snd_type (fst_type m), snd_type m)%core)%morphism)%core); simpl.
-      exact (fst_type (fst_type m)).
-  Defined.
-
-Local Open Scope functor_scope.
   Definition tri_monoidal_product_L : Functor (C * C * C) C
     := tensor_product o (tensor_product, 1).
-  Local Arguments tri_monoidal_product_L / .
-  Local Arguments compose / .
-  Eval simpl in tri_monoidal_product_L.
-  Eval cbv beta iota zeta delta [tri_monoidal_product_L compose] in tri_monoidal_product_L.
-    refine (_ o fst)%functor.
-  Proof.
-    refine (Build_Functor (C * C * C) C
-      tri_monoidal_product_L_object_of
-      tri_monoidal_product_L_morphism_of
-      _
-      _
-    ).
-    subst_body;
-    abstract (
-      intros; destruct_hypotheses; simpl;
-        repeat (rewrite <- FCompositionOf; unfold ProductCategory; simpl);
-          repeat rewrite FIdentityOf;
-            reflexivity
-    ).
-  Defined.
-
-  Definition tri_monoidal_product_R' : Functor (C * C * C) C.
-    refine (Build_Functor (C * C * C) C
-      tri_monoidal_product_R_object_of
-      tri_monoidal_product_R_morphism_of
-      _
-      _
-    );
-    subst_body;
-    abstract (
-      intros; destruct_hypotheses; simpl;
-        repeat (rewrite <- FCompositionOf; unfold ProductCategory; simpl);
-          repeat rewrite FIdentityOf;
-            reflexivity
-    ).
-  Defined.
-
-  Definition tri_monoidal_product_L'' : Functor (C * C * C) C.
-    simpl_definition_by_tac_and_exact tri_monoidal_product_L' ltac:(idtac; subst_body).
-  Defined.
-
-  Definition tri_monoidal_product_R'' : Functor (C * C * C) C.
-    simpl_definition_by_tac_and_exact tri_monoidal_product_R' ltac:(idtac; subst_body).
-  Defined.
-
-  (* Then we clean up a bit with reduction. *)
-  Definition tri_monoidal_product_L := Eval cbv beta iota zeta delta [tri_monoidal_product_L''] in tri_monoidal_product_L''.
-  Definition tri_monoidal_product_R := Eval cbv beta iota zeta delta [tri_monoidal_product_R''] in tri_monoidal_product_R''.
+  Definition tri_monoidal_product_R : Functor (C * C * C) C
+    :=  tensor_product o (1, tensor_product) o Associativity.inverse _ _ _.
 
   (**
      - an object [I] called the unit object or identity object,
    *)
-  Variable UnitObject : C.
-  Let I := UnitObject.
+  Variable unit_object : C.
+  Let I := unit_object.
   (**
      - three natural isomorphisms subject to certain coherence
          conditions expressing the fact that the tensor operation
        + is associative: there is a natural isomorphism [α], called
-           associator, with components [α_{A,B,C} : (A ⊗ B) ⊗ C ~= A ⊗ (B ⊗ C)],
+           associator, with components [α_{A,B,C} : (A ⊗ B) ⊗ C ≅ A ⊗ (B ⊗ C)],
     *)
-  Variable Associator : NaturalIsomorphism tri_monoidal_product_L tri_monoidal_product_R.
-  Let α := Associator.
+  Variable associator : NaturalIsomorphism tri_monoidal_product_L tri_monoidal_product_R.
+  Local Notation α := associator.
   (**
        + has [I] as left and right identity: there are two natural
            isomorphisms [λ] and [ρ], respectively called left and
            right unitor, with components
-           [λ A : I ⊗ A ~= A] and [ρ A : A ⊗ I ~= A].
+           [λ A : I ⊗ A ≅ A] and [ρ A : A ⊗ I ≅ A].
      *)
   Definition LeftUnitorFunctor : Functor C C.
-    clear tri_monoidal_product_L_morphism_of tri_monoidal_product_R_morphism_of
-      tri_monoidal_product_L_morphism_of' tri_monoidal_product_R_morphism_of'
-      tri_monoidal_product_L_morphism_of'' tri_monoidal_product_R_morphism_of''
-      tri_monoidal_product_L_object_of tri_monoidal_product_R_object_of.
+  Proof.
+    pose ((tensor_product o (!I, 1) o @ProductLaws.Law1.functor $(admit)$ _)).
+    pose .
     refine {| object_of := (fun A => I ⊗ A);
       morphism_of := (fun s d (m : morphism C s d) => Identity (C := C) I ⊗m m)
     |}; subst_body;
@@ -193,28 +127,28 @@ Local Open Scope functor_scope.
      commutes
      *)
 
-  Section AssociatorCoherenceCondition.
+  Section associatorCoherenceCondition.
     Variables a b c d : C.
 
     (* going from top-left *)
-    Let AssociatorCoherenceConditionT0 : morphism C (((a ⊗ b) ⊗ c) ⊗ d) ((a ⊗ (b ⊗ c)) ⊗ d)
+    Let associatorCoherenceConditionT0 : morphism C (((a ⊗ b) ⊗ c) ⊗ d) ((a ⊗ (b ⊗ c)) ⊗ d)
       := α (a, b, c) ⊗m Identity (C := C) d.
-    Let AssociatorCoherenceConditionT1 : morphism C ((a ⊗ (b ⊗ c)) ⊗ d) (a ⊗ ((b ⊗ c) ⊗ d))
+    Let associatorCoherenceConditionT1 : morphism C ((a ⊗ (b ⊗ c)) ⊗ d) (a ⊗ ((b ⊗ c) ⊗ d))
       := α (a, b ⊗ c, d).
-    Let AssociatorCoherenceConditionT2 : morphism C (a ⊗ ((b ⊗ c) ⊗ d)) (a ⊗ (b ⊗ (c ⊗ d)))
+    Let associatorCoherenceConditionT2 : morphism C (a ⊗ ((b ⊗ c) ⊗ d)) (a ⊗ (b ⊗ (c ⊗ d)))
       := Identity (C := C) a ⊗m α (b, c, d).
-    Let AssociatorCoherenceConditionB0 : morphism C (((a ⊗ b) ⊗ c) ⊗ d) ((a ⊗ b) ⊗ (c ⊗ d))
+    Let associatorCoherenceConditionB0 : morphism C (((a ⊗ b) ⊗ c) ⊗ d) ((a ⊗ b) ⊗ (c ⊗ d))
       := α (a ⊗ b, c, d).
-    Let AssociatorCoherenceConditionB1 : morphism C ((a ⊗ b) ⊗ (c ⊗ d)) (a ⊗ (b ⊗ (c ⊗ d)))
+    Let associatorCoherenceConditionB1 : morphism C ((a ⊗ b) ⊗ (c ⊗ d)) (a ⊗ (b ⊗ (c ⊗ d)))
       := α (a, b, c ⊗ d).
 
-    Definition AssociatorCoherenceCondition' :=
-      Compose AssociatorCoherenceConditionT2 (Compose AssociatorCoherenceConditionT1 AssociatorCoherenceConditionT0)
-      = Compose AssociatorCoherenceConditionB1 AssociatorCoherenceConditionB0.
-  End AssociatorCoherenceCondition.
+    Definition associatorCoherenceCondition' :=
+      Compose associatorCoherenceConditionT2 (Compose associatorCoherenceConditionT1 associatorCoherenceConditionT0)
+      = Compose associatorCoherenceConditionB1 associatorCoherenceConditionB0.
+  End associatorCoherenceCondition.
 
-  Definition AssociatorCoherenceCondition := Eval unfold AssociatorCoherenceCondition' in
-    forall a b c d : C, AssociatorCoherenceCondition' a b c d.
+  Definition associatorCoherenceCondition := Eval unfold associatorCoherenceCondition' in
+    forall a b c d : C, associatorCoherenceCondition' a b c d.
 
   (**
      - for all [A] and [B] in [C], the triangle diagram
@@ -246,11 +180,11 @@ Section MonoidalCategory.
      - three natural isomorphisms subject to certain coherence
          conditions expressing the fact that the tensor operation
        + is associative: there is a natural isomorphism [α], called
-           associator, with components [α_{A,B,C} : (A ⊗ B) ⊗ C ~= A ⊗ (B ⊗ C)],
+           associator, with components [α_{A,B,C} : (A ⊗ B) ⊗ C ≅ A ⊗ (B ⊗ C)],
        + has [I] as left and right identity: there are two natural
            isomorphisms [λ] and [ρ], respectively called left and
            right unitor, with components
-           [λ A : I ⊗ A ~= A] and [ρ A : A ⊗ I ~= A].
+           [λ A : I ⊗ A ≅ A] and [ρ A : A ⊗ I ≅ A].
      The coherence conditions for these natural transformations are:
      - for all [A], [B], [C] and [D] in [C], the pentagon diagram
      [[
@@ -298,7 +232,7 @@ Section MonoidalCategory.
   Let src (C : Category) {s d} (_ : morphism C s d) := s.
   Let dst (C : Category) s d (_ : morphism C s d) := d.
 
-  Let AssociatorCoherenceCondition' := Eval unfold AssociatorCoherenceCondition in @AssociatorCoherenceCondition.
+  Let associatorCoherenceCondition' := Eval unfold associatorCoherenceCondition in @associatorCoherenceCondition.
   Let UnitorCoherenceCondition' := Eval unfold UnitorCoherenceCondition in @UnitorCoherenceCondition.
 
   Record MonoidalCategory := {
@@ -308,7 +242,7 @@ Section MonoidalCategory.
 
     IdentityObject : MonoidalUnderlyingCategory where "'I'" := IdentityObject;
 
-    Associator : NaturalIsomorphism (tri_monoidal_product_L tensor_product) (tri_monoidal_product_R tensor_product) where "'α'" := Associator;
+    associator : NaturalIsomorphism (tri_monoidal_product_L tensor_product) (tri_monoidal_product_R tensor_product) where "'α'" := associator;
     LeftUnitor : NaturalIsomorphism (LeftUnitorFunctor tensor_product I)  (IdentityFunctor _) where "'λ'" := LeftUnitor;
     RightUnitor : NaturalIsomorphism (RightUnitorFunctor tensor_product I) (IdentityFunctor _) where "'ρ'" := RightUnitor;
 
@@ -326,7 +260,7 @@ Section MonoidalCategory.
                                            α_{A,B,C⊗D}
      ]]
      *)
-    AssociatorCoherent : AssociatorCoherenceCondition' α;
+    associatorCoherent : associatorCoherenceCondition' α;
     UnitorCoherent : UnitorCoherenceCondition' α λ ρ
   }.
 End MonoidalCategory.
